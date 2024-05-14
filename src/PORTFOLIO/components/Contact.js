06b0057e90d869261320e037b7adbc2568d7d800
siva-002 <vsivakumar971@gmail.com>
2024-05-14 20:22:14 +0530

    up

--- src/PORTFOLIO/components/Contact.js
+++ src/PORTFOLIO/components/Contact.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import "./css/Contact.css"
 import {FaEnvelope, FaInstagram, FaLinkedin, FaWhatsapp} from "react-icons/fa"
@@ -42,4 +43,49 @@ const Contact = () => {
   )
 }
 
+=======
+import React from 'react'
+import "./css/Contact.css"
+import {FaEnvelope, FaInstagram, FaLinkedin, FaWhatsapp} from "react-icons/fa"
+import contact from "./image/contact.png"
+import { motion } from 'framer-motion'
+const Contact = () => {
+  return (
+    <motion.div id="contact" className='contact'   
+    initial={{x:"100vw",opactity:0}}
+    animate={{x:0,opacity:1}}            
+  transition={{type:"spring",stiffness:"200"}}
+    >
+         <motion.div className='contact-con'
+            
+         >
+         <div className='socialmedia'>
+
+              <span className='title'>Contact me </span>
+              <div className='apps-image-con'>
+              <div className='apps'>
+                <div className='insta'> <a href="https://www.instagram.com/_evil_crown_holder_/" target="_blank"><span className='icon'><FaInstagram /> </span>Instagram</a></div>
+                <div className='linkedIn'><a href="https://www.linkedin.com/in/sivakumar-v-31b154273/" target="_blank"><span className='icon'><FaLinkedin/> </span>LinkedIn</a></div>
+                <div className='whatsapp'><a href="https://wa.link/46tym5" target="_blank"><span className='icon'><FaWhatsapp/></span> Whatsapp</a></div>
+                <div className='gmail'><a href="mailto:vsivakumar971@gmail.com" target="_blank"><span className='icon'><FaEnvelope/></span> Email</a></div>
+                <br/> 
+                </div>
+              <div className='image'>
+                  <img src={contact}/>
+              </div>
+                </div>
+             
+         </div>
+       
+         <div className='contact-name'>
+            {/* <span>Email me :</span> */}
+            <span> Developed By Sivakumar V</span> <br/>
+           
+         </div>
+         </motion.div>
+    </motion.div>
+  )
+}
+
+>>>>>>> 0d5f10f (up)
 export default Contact
--- src/PORTFOLIO/components/Index.js
+++ src/PORTFOLIO/components/Index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useState } from 'react'
 import "./css/index.css"
 import person from "./image/person.jpeg"
@@ -72,3 +73,80 @@ const Index = () => {
 }
 
 export default Index
+=======
+import React, { useEffect, useState } from "react";
+import "./css/index.css";
+import person from "./image/person.jpeg";
+import resume from "./documents/resume.pdf";
+import { motion } from "framer-motion";
+const Index = () => {
+  const [name, setname] = useState("");
+  const [profession, setprofession] = useState("");
+  const [nametype, setnametype] = useState(true);
+  const [professiontype, setprofessiontype] = useState(false);
+  useEffect(() => {
+    const namevalue = "I'm Sivakumar V";
+    const professionvalue = "Aspiring MERN Stack Developer";
+    const typing = () => {
+      if (name.length < namevalue.length) {
+        setname(namevalue.substring(0, name.length + 1));
+      } else {
+        setnametype(false);
+      }
+      if (
+        profession.length < professionvalue.length &&
+        name.length == namevalue.length
+      ) {
+        setprofessiontype(true);
+        setprofession(professionvalue.substring(0, profession.length + 1));
+      } else {
+        setprofessiontype(false);
+      }
+    };
+    const anim = setInterval(typing, 100);
+    return () => clearInterval(anim);
+  }, [name, profession]);
+  return (
+    <motion.div className="homepage" id="home"
+    initial={{ x: "100vw", opacity: 0 }}
+    animate={{ x: 0, opacity: 1, paddingTop: "25vh" }}
+    >
+      <div className="container-fluid ">
+        
+        <motion.div
+          className="col-container"
+        >
+          <div className="col-first mt-lg-5">
+            <span className="name">
+              <span className="hi">Hi </span>
+              {name} {nametype ? <span className="blink"></span> : ""}
+            </span>
+            <span className="profession">
+              {profession}
+              {professiontype ? <span className="blink"></span> : ""}
+            </span>
+
+            <span className="resume-btn">
+              <a href={resume} download="myresume.pdf">
+                <motion.button
+                  className=""
+                  whileTap={{ scale: "0.9" }}
+                  whileHover={{ scale: "1.1" }}
+                >
+                  Resume
+                </motion.button>
+              </a>
+            </span>
+          </div>
+
+          <div className="col-second">
+            <img className="d-none d-lg-block" src={person} />
+          </div>
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Index;
+>>>>>>> 0d5f10f (up)
